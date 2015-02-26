312560b96d7f4e2062a7b8d01a3f79c836aa9517
Sebastian Antosch <s.antosch@i-san.de>
2015-02-26 15:59:50 +0100

    [FEATURE] define an array for page errors

--- app/index.php
+++ app/index.php
@@ -23,6 +23,9 @@ require_once 'classes/config/config.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script>
+        var pageErrors = [];
+    </script>
     <?php
         $conf = Config::getInstance();
     ?>
