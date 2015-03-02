4f0f11250afd2a026bdb1b86abad7657cb699f3d
cbaur <c.baur@i-san.de>
2015-03-02 18:18:52 +0100

    provide documentation for the drag an drop directive

--- app/js/directives/dragAndDrop.js
+++ app/js/directives/dragAndDrop.js
@@ -1,16 +1,30 @@
+/**
+ * AngularJS directive for drag and drop functionality based on HTML5 drag and drop API.
+ */
 angular.module('ama')
     .directive('dragAndDrop', function () {
         return {
             restrict: 'A',
             link: function ($scope, elem, attr) {
+                /**
+                 * We listen for an event that triggers the binding of the dragstart event to the element.
+                 * Thus, new draggable elements can be created at runtime.
+                 */
                 $scope.$on('bindDragging', function (event, data) {
                     data.element.on('dragstart', function (e) {
+                        /**
+                         * save some information about the dragged element
+                         * the dataTransfer key has to be 'text' due to some weird IE restriction
+                         * the value must be a string
+                         */
                         e.originalEvent.dataTransfer.setData('text', JSON.stringify({element: e.target.outerHTML, fromDropArea: data.fromDropArea}));
-                        console.log(data);
                     });
 
                     data.element.on('dragend', function (e) {
                         var dragData = $.parseJSON(e.originalEvent.dataTransfer.getData('text'));
+                        /**
+                         * remove the dragged element from the DOM if it was taken from the drop (=target) area
+                         */
                         if(dragData.fromDropArea)
                             $(e.target).remove();
                     });
@@ -22,6 +36,9 @@ angular.module('ama')
         return {
             restrict: 'A',
             link: function($scope, elem, attr) {
+                /**
+                 * Trigger an event to bind the required event listeners to the element
+                 */
                 $scope.$emit('bindDragging', {element:elem});
             }
         };
@@ -31,7 +48,9 @@ angular.module('ama')
         return {
             restrict: 'A',
             link: function($scope, elem, attr) {
-
+                /**
+                 * The dragover event needs to be reset (preventDefault()). If not, the drop event will never fire.
+                 */
                 elem.on('dragover', function (e) {
                     e.preventDefault();
                 });
@@ -40,6 +59,9 @@ angular.module('ama')
                     e.preventDefault();
                 });
 
+                /**
+                 * When the drop event fires, copy the element to the drop area and re-bind the dragstart event to it
+                 */
                 elem.on('drop', function (e) {
                     e.preventDefault();
                     var dragged = $.parseJSON(e.originalEvent.dataTransfer.getData('text'));
