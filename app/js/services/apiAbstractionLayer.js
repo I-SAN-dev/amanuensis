04d8f763e5054b3da3d99a051cf56bdb7efe4042
cbaur <c.baur@i-san.de>
2015-03-11 16:40:13 +0100

    basic API abstraction Layer

---
+++ app/js/services/apiAbstractionLayer.js
@@ -0,0 +1,32 @@
+angular.module('ama')
+    .service('ApiAbstractionLayer', ['$http', 'constants', '$q', function ($http, constants, $q) {
+        var specialParams = {};
+        var apiUrl = (constants.SECUREURL || constants.BASEURL)+'api/';
+        specialParams.login = {
+            url: apiUrl+'login'
+        };
+
+        return function(method,identifier) {
+            var id = identifier;
+            if(typeof identifier == Object) {
+                id = identifier.name;
+            }
+            var config = {
+                method: method,
+                params: identifier.params,
+                data: identifier.data
+            };
+            $.extend(config, specialParams.id);
+
+            var defer = $q.defer();
+            $http(config)
+                .success(function (data) {
+                    defer.resolve(data);
+                })
+                .error(function (data) {
+                    defer.reject(data);
+                });
+            return defer.promise;
+
+        };
+    }]);
