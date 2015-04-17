16e99d37d6cde2b1329d2017f3207c07e5fc0694
cbaur <c.baur@i-san.de>
2015-04-17 11:31:50 +0200

    add client_data linking in clientDetail

--- app/js/controllers/clientDetailCtrl.js
+++ app/js/controllers/clientDetailCtrl.js
@@ -25,6 +25,47 @@ app.controller('ClientDetailCtrl',
                 $scope.$parent.setDetail({id:$stateParams.id});
             }
 
+            this.newConnectionFlag = {
+                phone: false,
+                mail: false
+            };
+
+            this.setNewConnectionFlag = function(type) {
+                self.newConnectionFlag[type] = !self.newConnectionFlag[type];
+            };
+
+            this.newConnection = {
+                phone: {
+                    name: '',
+                    value: ''
+                },
+                mail: {
+                    name: '',
+                    value: ''
+                }
+            };
+
+            this.addConnection = function(type) {
+                var name = self.newConnection[type].name;
+                var value = self.newConnection[type].value;
+                var data = {
+                    clientid: self.client.id,
+                    datatype: type,
+                    name: name,
+                    value: value
+                };
+                ApiAbstractionLayer('POST', {name: 'client_data', data: data}).then(function () {
+                    self.setNewConnectionFlag(type);
+                    var conns = self.client.data[type];
+                    if(conns) {
+                        conns.push({name: name, value: value});
+                    } else {
+                        self.client.data[type] = [{name: name, value: value}];
+                    }
+                    LocalStorage.setData('client/'+self.client.id, self.client);
+                });
+            };
+
         }
     ]
 );
