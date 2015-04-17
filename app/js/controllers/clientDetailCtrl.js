3d33ba41da126d6e3db478fdfb2f632544d50ca8
cbaur <c.baur@i-san.de>
2015-04-17 12:21:11 +0200

    comments & documentation on ClientDetailCtrl

--- app/js/controllers/clientDetailCtrl.js
+++ app/js/controllers/clientDetailCtrl.js
@@ -9,6 +9,11 @@ app.controller('ClientDetailCtrl',
         function (ApiAbstractionLayer, LocalStorage, $scope, $stateParams) {
 
             var self = this;
+
+            /**
+             * Gets a client from the API
+             * @param {int} id - the client id to query for
+             */
             var getClient = function(id) {
                 self.client = LocalStorage.getData('client'+'/'+id);
                 ApiAbstractionLayer('GET', {name:'client', params: {id:id}}).then(function (data) {
@@ -16,24 +21,40 @@ app.controller('ClientDetailCtrl',
                     self.client = data;
                 });
             };
+
+            // call getClient when the detail view is requested
             $scope.$on('detailChanged', function(event, data){
                 self.client = data;
                 getClient(data.id);
             });
 
+            // if the client state is entered with a certain id, we want to set the client detail for this id
             if($stateParams.id){
                 $scope.$parent.setDetail({id:$stateParams.id});
             }
 
+            // the next few lines contain code to create a new client - client_data connection
+
+            // set flags for all types of client data.
+            // the flag indicate if we currently want to add a new client data of its type
+            // TODO: do this dynamically
             this.newConnectionFlag = {
                 phone: false,
                 mail: false
             };
 
+            /**
+             * Toggles the flag for the newConnection creation mode
+             * @param {string} type - identifier of the type of client data
+             */
             this.setNewConnectionFlag = function(type) {
                 self.newConnectionFlag[type] = !self.newConnectionFlag[type];
             };
 
+            // just a simple object containing any possible new client data
+            // with (initially) empty names and values.
+            // bound to input fields in the view
+            // TODO: create this dynamically
             this.newConnection = {
                 phone: {
                     name: '',
@@ -45,6 +66,10 @@ app.controller('ClientDetailCtrl',
                 }
             };
 
+            /**
+             * Performs a database request to add a new client - client_data connection of a certain type
+             * @param {string} type - identifier of the type of client_data to be created
+             */
             this.addConnection = function(type) {
                 var name = self.newConnection[type].name;
                 var value = self.newConnection[type].value;
@@ -54,9 +79,11 @@ app.controller('ClientDetailCtrl',
                     name: name,
                     value: value
                 };
-                ApiAbstractionLayer('POST', {name: 'client_data', data: data}).then(function () {
+                // Post to the API
+                ApiAbstractionLayer('POST', {name: 'client_data', data: data}).then(function (data) {
                     self.setNewConnectionFlag(type);
                     var conns = self.client.data[type];
+                    // TODO: should push a return value here, API will support this soon
                     if(conns) {
                         conns.push({name: name, value: value});
                     } else {
