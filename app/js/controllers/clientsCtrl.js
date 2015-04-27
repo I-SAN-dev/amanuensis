5a627bad05f23692acc836a3448e89136e333869
cbaur <c.baur@i-san.de>
2015-04-27 12:47:25 +0200

    fix masterDetail problems with keyboard navigation

--- app/js/controllers/clientsCtrl.js
+++ app/js/controllers/clientsCtrl.js
@@ -11,13 +11,12 @@ app.controller('ClientsCtrl',
         'DeleteService',
         '$document',
         '$q',
-        function (ApiAbstractionLayer, LocalStorage, $scope, $stateParams, DeleteService, $document, $q) {
+        function (ApiAbstractionLayer, LocalStorage, $scope, $stateParams, DeleteService) {
             this.clientList = LocalStorage.getData('clients');
 
             this.filterText = '';
             var self = this;
 
-            var orderById = {};
 
             var setClientList = function (data) {
                 for(var i= 0; i<data.length; i++){
@@ -28,8 +27,6 @@ app.controller('ClientsCtrl',
                             +' '
                             +(data[i].contact_lastname || '');
                     }
-                    // save the list order for easy selection of next/prev item (keyboard navigation)
-                    orderById[data[i].id] = i;
                 }
                 self.clientList = data;
                 LocalStorage.setData('clients', self.clientList);
@@ -57,61 +54,6 @@ app.controller('ClientsCtrl',
                 });
             };
 
-            /**
-             * Selects a client from the list and passes the data over to the masterDetailCtrl
-             * @param {object} client - the client selected
-             */
-            this.setDetail = function (client) {
-                //var defer = $q.defer();
-                $stateParams.id = client.id;
-                $scope.$parent.setDetail(client);/*.then(function (result) {
-                    defer.resolve();
-                });
-                return defer.promise;*/
-            };
-
-            /**
-             * Keyboard navigation fr the client list
-             */
-            $document.on('keydown', function (event) {
-                var key = event.keyCode;
-                var oldId = angular.copy($stateParams.id);
-                var oldPos = orderById[oldId];
-
-                var viewportHeight = window.innerHeight;
-                var scrollTop = $document.scrollTop();
-                var viewPortBottom = viewportHeight + scrollTop;
-
-
-                if(key == 38 || key == 40){
-                    event.stopImmediatePropagation();
-                    event.preventDefault();
-                    var animation = {
-                        duration:500,
-                        queue:false
-                    };
-                    var documentOffset = 70;
-                    if (key == 38){
-                        if(self.clientList[oldPos - 1]) {
-                            var newActiveOffset = $('.list-group-item.active').prev('.list-group-item').offset().top;
-                            self.setDetail(self.clientList[oldPos - 1]);
-
-                            if(newActiveOffset < scrollTop)
-                                $('html, body').animate({scrollTop:newActiveOffset-documentOffset}, animation);
-                        }
-                    } else {
-                        if (self.clientList[oldPos + 1]) {
-                            var newActiveItem = $('.list-group-item.active').next('.list-group-item');
-                            var newActiveOffset = newActiveItem.height() + newActiveItem.offset().top;
-
-                            self.setDetail(self.clientList[oldPos + 1]);
-                            if(newActiveOffset > viewPortBottom) {
-                                $('html, body').animate({scrollTop: newActiveOffset-viewportHeight+documentOffset}, animation);
-                            }
-                        }
-                    }
-                }
-            });
 
         }
     ]
--- app/js/directives/masterDetail.js
+++ app/js/directives/masterDetail.js
@@ -2,28 +2,97 @@ app.directive('masterDetail', [function(){
     return {
         restrict: 'A',
         scope: {
+            masterList: '=',
             masterTpl: '=',
             detail: '=',
             detailTpl: '='
         },
+        transclude: true,
         templateUrl: 'templates/directives/masterDetail.html',
-        controller: function ($scope, $state, $stateParams ) {
-            console.log($scope.detail);
-            this.detail = $scope.detail;
-            this.masterTpl = $scope.masterTpl;
-            this.detailTpl = $scope.detailTpl;
-            var self = this;
-            $scope.setDetail = function(detail){
-                //var defer = $q.defer();
-                self.detail = detail;
-                $scope.$broadcast('detailChanged', detail);
-                $state.transitionTo($state.$current.name, {id: detail.id},{ location: true, inherit: true, relative: $state.$current, notify: false });
-            };
-            $scope.setDetailTpl = function(templateUrl) {
-                self.detailTpl = templateUrl;
-                $scope.$broadcast('detailTemplateChanged', self.detail);
-            };
-        },
+        controller: [
+            '$scope',
+            '$state',
+            '$stateParams',
+            '$document',
+            function ($scope, $state, $stateParams, $document) {
+                this.detail = $scope.detail;
+                this.masterTpl = $scope.masterTpl;
+                this.detailTpl = $scope.detailTpl;
+                var self = this;
+
+                $scope.setDetail = function(detail){
+                    $stateParams.id = detail.id;
+                    self.detail = detail;
+                    console.log(self.detail);
+                    $scope.$broadcast('detailChanged', detail);
+                    $state.transitionTo($state.$current.name, {id: detail.id},{ location: true, inherit: true, relative: $state.$current, notify: false });
+                };
+
+                var getNeighbor = function (offset) {
+                    var orderById = {};
+                    for(var i= 0; i<$scope.masterList.length;i++) {
+                        orderById[$scope.masterList[i].id] = i;
+                    }
+
+                    var oldPos = orderById[self.detail.id];
+
+                    return $scope.masterList[oldPos+offset];
+                };
+
+                $document.unbind('keydown');
+                $document.on('keydown', function (event) {
+                    var key = event.keyCode;
+
+
+
+                    var animation = {
+                        duration:500,
+                        queue:false
+                    };
+                    var documentOffset = 70;
+
+                    var viewportHeight = window.innerHeight;
+                    var scrollTop = $document.scrollTop();
+                    var viewPortBottom = viewportHeight + scrollTop;
+
+                    if (key == 38){
+                        event.stopPropagation();
+                        event.preventDefault();
+                        var prevDetail = getNeighbor(-1);
+
+                        if(prevDetail) {
+                            $scope.setDetail(getNeighbor(-1));
+
+                            var newActiveOffset = $('.list-group-item.active').prev('.list-group-item').offset().top;
+                            if(newActiveOffset < scrollTop)
+                                $('html, body').animate({scrollTop:newActiveOffset-documentOffset}, animation);
+                        }
+                    }
+                    if (key == 40){
+                        event.stopPropagation();
+                        event.preventDefault();
+                        var nextDetail = getNeighbor(1);
+                        if (nextDetail) {
+                            $scope.setDetail(getNeighbor(1));
+
+
+                            var newActiveItem = $('.list-group-item.active').next('.list-group-item');
+                            var newActiveOffset = newActiveItem.height() + newActiveItem.offset().top;
+
+                            if(newActiveOffset > viewPortBottom) {
+                                $('html, body').animate({scrollTop: newActiveOffset-viewportHeight+documentOffset}, animation);
+                            }
+                        }
+                    }
+                });
+
+                $scope.setDetailTpl = function(templateUrl) {
+                    self.detailTpl = templateUrl;
+                    $scope.$broadcast('detailTemplateChanged', self.detail);
+                };
+
+
+            }],
         controllerAs: 'MasterDetailCtrl'
     }
 }]);
