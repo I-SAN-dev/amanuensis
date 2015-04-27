c1813e71c21ac54f6849c7da01ab7628a59a06e9
cbaur <c.baur@i-san.de>
2015-04-27 16:24:02 +0200

    merge different types of contracts in a single array to show them in a list

--- app/js/controllers/projectDetailCtrl.js
+++ app/js/controllers/projectDetailCtrl.js
@@ -6,6 +6,7 @@ app.controller('ProjectDetailCtrl',
         '$stateParams',
         function (ApiAbstractionLayer, LocalStorage, $scope, $stateParams) {
             var self = this;
+            this.contracts = [];
 
             /**
              * Gets a project from the API
@@ -13,15 +14,30 @@ app.controller('ProjectDetailCtrl',
              */
             var getProject = function(id) {
                 self.project = LocalStorage.getData('project'+'/'+id);
+                self.contracts = LocalStorage.getData('project/'+id+'/contracts');
                 ApiAbstractionLayer('GET', {name:'project', params: {id:id}}).then(function (data) {
                     LocalStorage.setData('project'+'/'+id, data);
                     self.project = data;
+                    var contracts = [];
+                    for (var i = 0; i < data.contracts.length; i++) {
+                        var contract = data.contracts[i];
+                        contract.type = 'app';
+                        contracts.push(contract);
+                    }
+                    for (var j=0; j < data.fileContracts.length; j++) {
+                        contract = data.fileContracts[j];
+                        contract.type = 'file';
+                        contracts.push(contract);
+                    }
+                    self.contracts = contracts;
+                    LocalStorage.setData('project/'+id+'/contracts', self.contracts);
                 });
             };
 
             // call getProject when the detail view is requested
             $scope.$on('detailChanged', function(event, data){
                 self.project = data;
+                self.contracts = [];
                 getProject(data.id);
             });
 
