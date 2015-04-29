12a9e571d34608d773fa85867c36e0a11bb13075
cbaur <c.baur@i-san.de>
2015-04-29 10:35:36 +0200

    remove left menu

--- app/index.php
+++ app/index.php
@@ -54,12 +54,7 @@ ob_start();
     <div class="container-fluid" data-ama-module="'actionBar'"></div>
 </div>
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-1 col-md-2 col-sm-3">
-            <div data-ama-menu data-menuname="'mainNav'"></div>
-        </div>
-        <div class="col-lg-11 col-md-10 col-sm-9" data-ui-view="mainContent" id="mainContent"></div>
-    </div>
+    <div data-ui-view="mainContent" id="mainContent"></div>
 
 </div>
 
