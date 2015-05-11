bb6688ee6b82595a83a9b224d540eda929c070f1
cbaur <c.baur@i-san.de>
2015-05-11 22:17:46 +0200

    create view for the first few settings. it'll get tough to include all these options (because this type of work is so boring)

--- app/js/controllers/settingsCtrl.js
+++ app/js/controllers/settingsCtrl.js
@@ -1,6 +1,13 @@
 app.controller('SettingsCtrl', [
+    'ApiAbstractionLayer',
+    'LocalStorage',
     'constants',
-    function (constants) {
+    function (ApiAbstractionLayer, LocalStorage, constants) {
+        var self = this;
+        this.settings = LocalStorage.getData('settings');
+        ApiAbstractionLayer('GET', 'settings').then(function (data) {
+            self.settings = data;
+        });
         this.company = constants.COMPANY;
         this.language = constants.LANGUAGE;
     }
