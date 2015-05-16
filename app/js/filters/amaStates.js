ed79894180f646a8632530506a3145c12d6b2ab6
cbaur <c.baur@i-san.de>
2015-05-16 10:48:15 +0200

    better error messages

--- app/js/filters/amaStates.js
+++ app/js/filters/amaStates.js
@@ -6,6 +6,22 @@ app.filter('amaStates', function () {
             '2': 'offers.states.pdfSent',
             '3': 'offers.states.clientAccepted',
             '-1': 'offers.states.clientDeclined'
+        },
+        error: {
+            "400": "errors.codes.400.message",
+            "401": "errors.codes.401.message",
+            "403": "errors.codes.403.message",
+            "404": "errors.codes.404.message",
+            "405": "errors.codes.405.message",
+            "500": "errors.codes.500.message"
+        },
+        errorDescription: {
+            "400": "errors.codes.400.description",
+            "401": "errors.codes.401.description",
+            "403": "errors.codes.403.description",
+            "404": "errors.codes.404.description",
+            "405": "errors.codes.405.description",
+            "500": "errors.codes.500.description"
         }
     };
 
