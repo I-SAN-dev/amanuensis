71f663956fa8fc52465e7e2549620d8b6280c0d2
Sebastian Antosch <s.antosch@i-san.de>
2015-05-22 17:14:02 +0200

    [FEATURE] automatic setting creation - todo: make it editable

--- app/js/controllers/settingsCtrl.js
+++ app/js/controllers/settingsCtrl.js
@@ -5,12 +5,50 @@ app.controller('SettingsCtrl', [
     '$scope',
     function (ApiAbstractionLayer, LocalStorage, constants, $scope) {
         var self = this;
+
+        /**
+         * Will give me all keys of an object
+         * @param obj
+         * @returns {Array}
+         */
+        this.objectKeys = function(obj)
+        {
+            if(obj)
+            {
+                return Object.keys(obj);
+            }
+            return [];
+        };
+
+        /**
+         * test if the given object can be a setting
+         * @param val
+         * @returns {boolean}
+         */
+        this.isSetting = function(val)
+        {
+            return((val != '%spacer%') && !(typeof(val) === 'object'));
+        };
+        /**
+         * Test if the given object is a setting object
+         * @param val
+         * @returns {boolean}
+         */
+        this.isSettingSet = function(val)
+        {
+            return((val != '%spacer%') && (typeof(val) === 'object'));
+        };
+
         this.settings = LocalStorage.getData('settings');
+        this.settingkeys = this.objectKeys(this.settings);
+
         ApiAbstractionLayer('GET', 'settings').then(function (data) {
             self.settings = data;
+            self.settingkeys = self.objectKeys(self.settings);
         });
-        this.company = constants.COMPANY;
-        this.language = constants.LANGUAGE;
+
+
+
 
 
 
