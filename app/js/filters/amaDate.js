7090e81c2ead6238b11424653730b8a8ab5dfb7a
cbaur <c.baur@i-san.de>
2015-05-24 14:01:47 +0200

    fix invalid date error in firefox

--- app/js/filters/amaDate.js
+++ app/js/filters/amaDate.js
@@ -1,6 +1,8 @@
 app.filter('amaDate', ['$filter', function ($filter) {
     return function (input, dateTimeFormat) {
-        if(input)
-            return $filter('date')(new Date(input).toISOString(), dateTimeFormat);
+        if(input){
+            input = input.replace(/\s/g, "T");
+            return $filter('date')(input, dateTimeFormat);
+        }
     };
 }]);
