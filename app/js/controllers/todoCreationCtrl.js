d0a89c410ab0ab43e0350a511f559619267dbdc5
cbaur <c.baur@i-san.de>
2015-06-01 19:28:24 +0200

    show error when user refreshes browser on todoCreation

--- app/js/controllers/todoCreationCtrl.js
+++ app/js/controllers/todoCreationCtrl.js
@@ -1,9 +1,14 @@
 app.controller('TodoCreationCtrl', [
     'ItemContainerService',
+    'ErrorDialog',
     '$state',
     '$stateParams',
-    function (ItemContainerService,$state,$stateParams) {
+    function (ItemContainerService,ErrorDialog,$state,$stateParams) {
         var self = this;
+        if(!$stateParams.project){
+            ErrorDialog({code:'1337',languagestring:'errors.noProjectSpecified'}).activate();
+            $state.go('app.dashboard')
+        }
         var project = $stateParams.project;
         var projectId = project.id;
         this.projectName = project.name;
