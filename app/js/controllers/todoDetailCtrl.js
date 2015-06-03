fb76d1d9cb742cf15948b22b869385d78925ede0
cbaur <c.baur@i-san.de>
2015-06-03 11:17:10 +0200

    register masterController in MasterDetailService for todo view

--- app/js/controllers/todoDetailCtrl.js
+++ app/js/controllers/todoDetailCtrl.js
@@ -1,9 +1,11 @@
 app.controller('TodoDetailCtrl', [
     'ApiAbstractionLayer',
     'LocalStorage',
+    'MasterDetailService',
     '$stateParams',
-    function (ApiAbstractionLayer, LocalStorage, $stateParams) {
+    function (ApiAbstractionLayer, LocalStorage, MasterDetailService, $stateParams) {
         var self = this;
+        MasterDetailService.setMaster(this);
         var id = $stateParams.id;
         this.todo = LocalStorage.getData('todo/'+ id);
         ApiAbstractionLayer('GET', {name:'todo',params: {id:id}}).then(function (data) {
