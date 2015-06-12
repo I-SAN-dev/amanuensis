6f1d3409d0d035e6128bf70b32b23eabfb7eda8f
Sebastian Antosch <s.antosch@i-san.de>
2015-06-12 10:33:19 +0200

    [IMPROVE] make sure only one error modal is shown at the same time, preventing multiple errors

--- app/js/services/errorDialog.js
+++ app/js/services/errorDialog.js
@@ -6,25 +6,48 @@
  * @returns {btfModal} The modal with the error information. Has to be activated to be shown (modal.activate()).
  */
 app.factory('ErrorDialog', function (btfModal) {
+
+
+    var self = this;
+    self.errorActive = false;
+
     return function(errorObj){
-        var modal = btfModal({
-            templateUrl: 'templates/modules/errorDialog.html',
-            controller: function(){
-                this.code = errorObj.code;
-                this.message = errorObj.message;
-                this.languagestring = errorObj.languagestring;
-                this.file = errorObj.file;
-                this.line = errorObj.line;
+
+        /* only show an error modal, if no other error modal is present */
+        if(!self.errorActive)
+        {
+            self.errorActive = true;
+            var modal = btfModal({
+                templateUrl: 'templates/modules/errorDialog.html',
+                controller: function(){
+                    this.code = errorObj.code;
+                    this.message = errorObj.message;
+                    this.languagestring = errorObj.languagestring;
+                    this.file = errorObj.file;
+                    this.line = errorObj.line;
+
+                    this.close = function () {
+                        console.log('close');
+                        self.errorActive = false;
+                        modal.deactivate();
+                    };
+                },
+                controllerAs: 'error'
+            });
+            return modal;
+        }
+        /* otherwise only log the error to the console */
+        else
+        {
+            /* return an object behaving similiar to the modal, so we can use it in same context */
+            /* quak quak ducktyping */
+            return {
+                activate: function(){
+                    console.error(errorObj.code + ' - ' + errorObj.message + ' (' + errorObj.file + ':' + errorObj.line + ')');
+                }
+            }
+        }
 
 
-
-                this.close = function () {
-                    console.log('close');
-                    modal.deactivate();
-                };
-            },
-            controllerAs: 'error'
-        });
-        return modal;
     };
 });
