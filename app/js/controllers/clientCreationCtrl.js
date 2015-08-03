7bc3d37c70735495fa4735f20d9f0f9741089504
Sebastian Antosch <s.antosch@i-san.de>
2015-08-03 17:15:07 +0200

    [FEATURE] First rough setup based on owncloud setup

--- app/js/controllers/clientCreationCtrl.js
+++ app/js/controllers/clientCreationCtrl.js
@@ -69,6 +69,7 @@ app.controller('ClientCreationCtrl',
                 self.allCategories = data;
                 LocalStorage.setData('clientCategories', data)
             });
+
             /**
              * An array containing all selected categories for the new client.
              * @type {Array}
